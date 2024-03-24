64ad2c07e8d86bfe8ece1ac16bcb6b7949fcc565
Sruthi Damera <sruthidamera@Sruthis-MacBook-Air.local>
2024-03-23 20:23:35 -0400

    add confirmation pop up

--- frontend/src/layouts/userRequest/index.js
+++ frontend/src/layouts/userRequest/index.js
@@ -2,7 +2,11 @@ import React, { useEffect, useState } from "react";
 import {
   Container,
   Card,
-  Button
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter
 } from 'reactstrap';
 import {user_requests,delete_request,signup, signup_approve} from '../../../src/api'
 
@@ -16,6 +20,9 @@ const UserRequest = () => {
   const [users, setUsers] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [mustGetUsers, setMustGetUsers] = useState(true);
+  const [modal, setModal] = useState(false);
+  const [approvalStatus, setApprovalStatus] = useState('');
+  const [UserNameOfRequest, UserNameOfRequesr] = useState('');
 
   useEffect(() => {
     if (mustGetUsers) {
@@ -37,6 +44,9 @@ const UserRequest = () => {
       console.log(error);
     });
   }
+
+  const toggleModal = () => setModal(!modal);
+
   const handleApprove = (userTemp) => {
  
 
@@ -53,9 +63,7 @@ const UserRequest = () => {
 
       console.log(userData);
 
-      delete_request(userTemp.id).then((req)=>{
-          console.log('deleted');
-    })
+     
  
     signup(userData)
       .then((response) => {
@@ -65,21 +73,41 @@ const UserRequest = () => {
         console.log(error.response.data);
       });
 
+      delete_request(userTemp.id).then((req)=>{
+        setApprovalStatus('approved');
+        toggleModal();
+        setTimeout(() => {
+          window.location.reload();
+        }, 4000);
+  })
 
 
  
   };
 
-  const handleDeny = (userId) => {
-    // // Handle denial logic
-    // denyUser(userId)
-    //   .then((response) => {
-    //     // If successful, refresh users data
-    //     setMustGetUsers(true);
-    //   })
-    //   .catch((error) => {
-    //     console.log(error);
-    //   });
+  const handleDeny = (userTemp) => {
+    const userData= {
+      'email': userTemp.email,
+        
+        'first_name': userTemp.first_name,
+        'last_name': userTemp.last_name,
+        'is_active':userTemp.is_active,
+        'user_type':userTemp.user_type,
+        'church': userTemp.church,
+        'password':userTemp.password
+      }
+
+      console.log(userData);
+
+      delete_request(userTemp.id).then((req)=>{
+          setApprovalStatus('denied');
+          window.location.reload();
+          toggleModal();
+        setTimeout(() => {
+          window.location.reload();
+        }, 4000);
+    })
+
   };
 
   return (
@@ -108,7 +136,7 @@ const UserRequest = () => {
                         <td style={{ padding: '8px' }}>{user.email}</td>
                         <td style={{ padding: '8px' }}>
                           <Button onClick={() => handleApprove(user)} color="success">Approve</Button>{' '}
-                          <Button onClick={() => handleDeny(user.id)} color="danger">Deny</Button>
+                          <Button onClick={() => handleDeny(user)} color="danger">Deny</Button>
                         </td>
                       </tr>
                     ))}
@@ -118,6 +146,16 @@ const UserRequest = () => {
             )}
           </div>
         </Card>
+
+        <Modal isOpen={modal} toggle={toggleModal}>
+        <ModalHeader toggle={toggleModal}>{approvalStatus === 'approved' ? 'Access Approved' : 'Access Denied'}</ModalHeader>
+        <ModalBody>
+          {approvalStatus === 'approved' ? 'User access has been approved.' : 'User access has been denied.'}
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={toggleModal}>OK</Button>{' '}
+        </ModalFooter>
+      </Modal>
       </Container>
     </div>
   );
